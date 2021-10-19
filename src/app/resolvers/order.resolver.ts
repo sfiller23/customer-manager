c8c270d771e106206864fdcd9ca2c8b47248acb8
sfiller23 <sf27lp@gmail.com>
2021-10-19 22:19:19 +0300

    view order page

---
+++ src/app/resolvers/order.resolver.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpService } from '../services/http.service';
+import { Customer } from '../interfaces/customer';
+import { Product } from '../interfaces/product';
+import { OrderDetails } from '../interfaces/orderDetails';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderResolver implements Resolve<OrderDetails> {
+
+  constructor(private httpService: HttpService) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<OrderDetails> {
+
+      const id = route.paramMap.get("id");
+
+      const orderDetails: OrderDetails = {
+        firstName: '',
+        lastName: '',
+        productsNames: [],
+        productsPrices: [],
+        totalSum: 0,
+      }
+
+      let orderDetails$: Observable<typeof orderDetails> = new Observable<typeof orderDetails>();
+
+      if(id){
+        orderDetails$ = this.httpService.getItemC('customers',id).pipe(map(customer=>{
+            let sum = 0;
+            orderDetails.firstName = customer.firstName;
+            orderDetails.lastName = customer.lastName;
+            this.httpService.getItems('products',customer.products).subscribe(products=>{
+              let sum = 0;
+              console.log(products);
+              products.forEach(product=>{
+                console.log(product.name);
+                orderDetails.productsNames?.push(product.name);
+                orderDetails.productsPrices?.push(product.price);
+                sum += product.price;
+              });
+              orderDetails.totalSum = sum;
+
+            });
+            return orderDetails;
+          }))
+      }
+
+
+
+      return orderDetails$;
+  }
+}
